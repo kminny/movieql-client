19accb8a02c20c43695da91d03f948faf5deefe6
kminny <darkdevil94@naver.com>
2019-03-23 21:12:00 +0900

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom"; 
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <ApolloProvider client={client}>
+        <Router>
+          <main>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+          </main>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/apolloClient.js
@@ -0,0 +1,7 @@
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://13.124.254.140:4000"
+});
+
+export default client;
